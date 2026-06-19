8a4460ed83ed65e0b58239d66c5ef0cd6f5c5b90
AmoHatef <khoshtipbalahatef@gmail.com>
2026-06-19 19:55:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from bale import Bot , Message
 import time
+import asyncio
 bot = Bot (token = '1119681388:qtZI2h3TJ4zlWdC4cuUismy18pBIMm4S72U')
 A = 0
 marhale = 0
@@ -23,7 +24,7 @@ async def on_message(pm = Message):
                 await bot.send_message(chat_id = '4862063605' , text = 'عاب بخور💦')
                 await bot.send_message(chat_id = '282784158' , text = 'عاب بخور💦')
                 await bot.send_message(chat_id = '1652444615' , text = 'عاب بخور💦')
-                time.sleep(1800)
+                asyncio.sleep(1800)
             
 
 if __name__ == '__main__':
